eb793e31804f8ac7a22a0a7ae080177b1cf001c7
Steven Wittens <steven@acko.net>
2021-04-28 03:25:35 +0200

    make it work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Headspace VSCode extension
 
+**Commands**
+- Run command "Start Headspace server" to start a server on port 44100
+- Run command "Stop Headspace server" to stop server
--- extension.js
+++ extension.js
@@ -15,6 +15,15 @@ const init = () => {
   if (!headspace) headspace = makeServer();
 }
 
+// Gather some action stats
+const actions = {
+  text: 0,
+  file: 0,
+  save: 0,
+};
+
+const countEvent = (type) => () => actions[type]++;
+
 // Measure activity/hazard parameters
 const measure = () => {
   let warnings = 0;
@@ -29,20 +38,35 @@ const measure = () => {
       if (severity === 1) hints++;
     }
   }
-    
+   
+  // Hazard goes from 0 -> 25% -> 50% -> 100% over hint, warning and error.
   const hazard = errors ? 1 : warnings ? 0.5 : hints ? 0.25 : 0;
 
+  // Activity is based on accumulated / decaying actions.
+  let {text, file, save} = actions;
+  const activity = Math.min(1, 
+    (1 - 1 / (save/2 + 1)) +
+    (1 - 1 / (file/2 + 1)) +
+    (1 - 1 / (text/16 + 1))
+  );
+
+  // Decay actions slowly
+  actions.text = Math.max(0, actions.text * .7);
+  actions.file = Math.max(0, actions.file * .95);
+  actions.save = Math.max(0, actions.save * .9);
+  
+  // console.log({text, file, save, activity});
+
   return {
-    activity: 0,
+    activity,
     hazard,
+    time: 5,
   };
 }
 
 // Notify listeners of new parameters
 const notify = (listeners) => {
-  console.log('notify', listeners)
   const parameters = measure();
-  console.log('measure', parameters)
   const data = `data: ${JSON.stringify(parameters)}\n\n`;
   for (let l of listeners) l.write(data);
 }
@@ -50,11 +74,13 @@ const notify = (listeners) => {
 // Shut down server
 const dispose = () => {
   if (headspace) {
+    vscode.window.showInformationMessage(`Headspace server stopped`);
     headspace.stop();
-    headspace.close();
+    try {
+      headspace.close();
+    } catch (e) {};
     headspace = null;
     console.log(`Headspace server stopped`);
-    vscode.window.showInformationMessage(`Headspace server stopped`);
   }  
 }
 
@@ -88,24 +114,44 @@ const makeServer = () => {
   app.listen(PORT, () => {
     console.log(`Headspace server at http://localhost:${PORT}`)
 		vscode.window.showInformationMessage(`Headspace server running at http://localhost:${PORT}`);
+    vscode.env.openExternal(vscode.Uri.parse(`https://headspace.acko.net/#listen=:${PORT}`));
   });
 
-  const timer = setInterval(() => notify(listeners), 1000);
-  const stop = () => clearInterval(timer);
+  const timer = setInterval(() => notify(listeners), 2000);
+  const stop = () => {
+    clearInterval(timer);
+    for (let l of listeners) try { l.close(); } catch (e) {};
+  }
   app.stop = stop;
 
   return app;
 }
 
 function activate(context) {
-  {
-  	let disposable = vscode.commands.registerCommand('extension.headspace.start', init);
-  	context.subscriptions.push(disposable);
-  }
-  {
-  	let disposable = vscode.commands.registerCommand('extension.headspace.stop', dispose);
-  	context.subscriptions.push(disposable);
-  }
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.headspace.start', init)
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.headspace.stop', dispose)
+  );
+  context.subscriptions.push(
+		vscode.workspace.onDidOpenTextDocument(countEvent('text'))
+	);
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeTextDocument(countEvent('text'))
+	);
+	context.subscriptions.push(
+		vscode.workspace.onDidSaveTextDocument(countEvent('save'))
+	);
+	context.subscriptions.push(
+		vscode.workspace.onDidCreateFiles(countEvent('file'))
+	);
+  context.subscriptions.push(
+		vscode.workspace.onDidRenameFiles(countEvent('file'))
+	);
+  context.subscriptions.push(
+		vscode.workspace.onWillDeleteFiles(countEvent('file'))
+	);
 }
 
 function deactivate() {
